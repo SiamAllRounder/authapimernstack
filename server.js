d2e7b5f83fb12c11ee9e4349caa43b9c69089000
siam <seum.bdit@gmail.com>
2023-12-08 23:00:05 +0600

    ALHAAMDULILLAH register api build completed

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const routes = require("./routes");
+const bodyParser = require("body-parser");
 const ExpressServer = express();
 require("dotenv").config();
 require("./config/db");
 
 const PORT = process.env.PORT || 8080;
+
+// IT is a middle ware this is very important for parse body
+ExpressServer.use(bodyParser.json());
 
 ExpressServer.use("/api/v1", routes);
 
--- userController/index.js
+++ userController/index.js
@@ -1,6 +1,25 @@
+const UserModel = require("../models/UserModel");
+const bcrypt = require("bcrypt");
 module.exports = {
-  registerUser: (req, res) => {
-    res.send("ALHAAMDULILLAH register success");
+  // validate req.body - Done
+  // create MongoDB UserModel - Done
+  // do password encryption - Done
+  // save data to mongodb
+  registerUser: async (req, res) => {
+    // if uncomment the line below node mon may crash
+    // res.send("ALHAAMDULILLAH register success");
+    const userModel = new UserModel(req.body);
+    userModel.password = await bcrypt.hash(req.body.password, 10);
+    try {
+      const response = await userModel.save();
+      // be aware never expose password to client protect it like this way
+      response.password = undefined;
+      return res
+        .status(201)
+        .json({ message: "ALHAAMDULILLAH success", data: response });
+    } catch (error) {
+      return res.status(500).json({ message: "error", error });
+    }
   },
   loginUser: (req, res) => {
     res.send("ALHAAMDULILLAH login success");
