1a5bee4365899264994fabcc024f1fe8a2c719f5
siam <seum.bdit@gmail.com>
2023-12-08 21:27:39 +0600

    ma sha ALLAH first model user model created

---
+++ models/UserModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = moongoose.Schema;
+
+const UserSchema = new Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const UserModel = mongoose.model("users", UserSchema);
+
+module.exports = UserModel;
